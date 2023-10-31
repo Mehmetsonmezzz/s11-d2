17c918734c2ebecba7c4913d3c16638cfc61adf3
Mehmet <mehmetsonmez216@gmail.com>
2023-11-01 02:47:26 +0300

    add PrivateRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import "./App.css";
+import { useState } from "react";
 import Login from "./components/Login";
-import Header from "./layouts/Header";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
+import { Switch, Route, Link } from "react-router-dom";
 function App() {
+  const [loggindUser, setLoggindUser] = useState({});
   return (
     <div className="App">
       <Header />
-      <Login />
+
+      <Switch>
+        <PrivateRoute auth={loggindUser} path="/friends/add/">
+          <div>friendadd form</div>
+        </PrivateRoute>
+        <PrivateRoute auth={loggindUser} path="/friends/">
+          <div>friends div</div>
+        </PrivateRoute>
+        <Route path="/login/">
+          <Login />
+        </Route>
+        <Route path="/logout/">
+          <div>Logout</div>
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {Route,Redirect} from "react-router-dom"
+export default function PrivateRoute({ children, ...rest }) {
+    //let auth = useAuth();
+    const {auth}=rest;
+    return (
+      <Route
+        {...rest}
+        render={({ location }) =>
+          auth.user ? (
+            children
+          ) : (
+            <Redirect
+              to={{
+                pathname: "/login",
+                state: { from: location }
+              }}
+            />
+          )
+        }
+      />
+    );
+  }
+
